bf63733feeaef2dd587e5a181744c8f972b2debe
Epimeria <61943481+Epimeria@users.noreply.github.com>
2022-10-01 00:15:39 -0500

    camelCase, remove redundancy

--- JS/app.js
+++ JS/app.js
@@ -1,11 +1,15 @@
 console.log("Welcome to notes app. This is app.js");
 showNotes();   // takes all the notes from local storage and display in notes is Div
+let addNoteButton = document.getElementById("addNoteButton");
 
-let addBtn = document.getElementById("addBtn");
-addBtn.addEventListener("click", function (e) {
-    let noteTextInputField = document.getElementById("addTxt");
-    let notes = localStorage.getItem("notes");
-    notesObj = JSON.parse(notes);
+function getNotes() {
+    maybeNotesFromLocalStorage = localStorage.getItem("notes")
+    return maybeNotesFromLocalStorage ? JSON.parse(maybeNotesFromLocalStorage) : []
+}
+
+addNoteButton.addEventListener("click", function (e) {
+    let noteTextInputField = document.getElementById("noteTextInputField");
+    let notesObj = getNotes()
     noteTextInputField.value ? notesObj.push(noteTextInputField.value) : console.log('Input field empty, ignoring click')
     localStorage.setItem("notes", JSON.stringify(notesObj));
     noteTextInputField.value = "";
@@ -14,10 +18,9 @@ addBtn.addEventListener("click", function (e) {
 
 // function to show elements from local storage 
 function showNotes() {
-    let notes = localStorage.getItem("notes");
-    notesObj = JSON.parse(notes);
+    let notesObj = getNotes()
     let html = "";
-    notesObj.forEach(function (element, index) {
+    notesObj.forEach((element, index) => {
         html += `
         <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
         <div class="card-body">
@@ -29,14 +32,12 @@ function showNotes() {
     });
     let notesElm = document.getElementById("notes");
     notesElm.innerHTML = html;
-
 }
 
 // function to delete a Note 
 function deleteNote(index) {
     console.log("I am deleting", index);
-    let notes = localStorage.getItem("notes");
-    notesObj = JSON.parse(notes);
+    let notesObj = getNotes()
     notesObj.splice(index, 1);
     localStorage.setItem("notes", JSON.stringify(notesObj));
     showNotes();
@@ -47,12 +48,8 @@ search.addEventListener("input", function () {
     let inputVal = search.value.toLowerCase();
     console.log("Input event fired!", inputVal);
     let noteCards = document.getElementByClassName('noteCard');
-    Array.from(noteCards).forEach(function (element) {
-        let cardTxt = element.getElementByTagName("p")[0].innerText;
-        if (cardTxt.include(inputVal)) {
-            element.style.display = "block";
-        } else {
-            element.style.display = "none";
-        }
+    Array.from(noteCards).forEach((element) => {
+        element.getElementByTagName("p")[0].innerText.include(inputVal) ? element.style.display = "block" : element.style.display = "none";
+
     })
 })
