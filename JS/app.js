176ce262b6bebebfb4cc770b7d49151a08713093
Epimeria <61943481+Epimeria@users.noreply.github.com>
2022-09-30 23:56:09 -0500

    Prevent empty note addition and cleanup

--- JS/app.js
+++ JS/app.js
@@ -2,35 +2,22 @@ console.log("Welcome to notes app. This is app.js");
 showNotes();   // takes all the notes from local storage and display in notes is Div
 
 let addBtn = document.getElementById("addBtn");
-addBtn.addEventListener("click", function(e){
-    let addTxt = document.getElementById("addTxt");
+addBtn.addEventListener("click", function (e) {
+    let noteTextInputField = document.getElementById("addTxt");
     let notes = localStorage.getItem("notes");
-    if(notes == null){
-        notesObj= [];
-    }
-    else{
-        notesObj = JSON.parse(notes);
-    }
-    notesObj.push(addTxt.value);
+    notesObj = JSON.parse(notes);
+    noteTextInputField.value ? notesObj.push(noteTextInputField.value) : console.log('Input field empty, ignoring click')
     localStorage.setItem("notes", JSON.stringify(notesObj));
-    addTxt.value = "";
-    console.log(notesObj);
-
+    noteTextInputField.value = "";
     showNotes();
-
 });
 
 // function to show elements from local storage 
-function showNotes(){
+function showNotes() {
     let notes = localStorage.getItem("notes");
-    if(notes == null){
-        notesObj= [];
-    }
-    else{
-        notesObj = JSON.parse(notes);
-    }
+    notesObj = JSON.parse(notes);
     let html = "";
-    notesObj.forEach(function(element, index) {
+    notesObj.forEach(function (element, index) {
         html += `
         <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
         <div class="card-body">
@@ -39,49 +26,33 @@ function showNotes(){
           <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
         </div>
       </div> `;
-
-
     });
     let notesElm = document.getElementById("notes");
-    if(notesObj.length != 0){
-        notesElm.innerHTML = html;
-    }
-    else{
-        notesElm.innerHTML = ``;
-    }
+    notesElm.innerHTML = html;
+
 }
 
 // function to delete a Note 
-function deleteNote(index){
-    console.log("I am deleating", index);
-
+function deleteNote(index) {
+    console.log("I am deleting", index);
     let notes = localStorage.getItem("notes");
-    if(notes == null){
-        notesObj= [];
-    }
-    else{
-        notesObj = JSON.parse(notes);
-    }
-
+    notesObj = JSON.parse(notes);
     notesObj.splice(index, 1);
     localStorage.setItem("notes", JSON.stringify(notesObj));
     showNotes();
 }
 
 let search = document.getElementById("searchTxt");
-search.addEventListener("input", function(){
+search.addEventListener("input", function () {
     let inputVal = search.value.toLowerCase();
     console.log("Input event fired!", inputVal);
     let noteCards = document.getElementByClassName('noteCard');
-    Array.from(noteCards).forEach(function(element){
+    Array.from(noteCards).forEach(function (element) {
         let cardTxt = element.getElementByTagName("p")[0].innerText;
-        if(cardTxt.include(inputVal)){
+        if (cardTxt.include(inputVal)) {
             element.style.display = "block";
-
         } else {
             element.style.display = "none";
         }
-       // console.log(cardTxt);
-
     })
 })
